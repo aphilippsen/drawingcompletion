5fa226f4afcc8cba0d2483b5592176337be17be6
Anja Philippsen <anja@nict.go.jp>
2019-12-18 10:43:17 +0900

    plot IS and UH history

--- test_plot.py
+++ test_plot.py
@@ -7,6 +7,10 @@ data_set_name = '2019-11-all'
 setting = "pca-only-one-norm"
 eval_dir = "./results/evaluation/" + data_set_name + "/"
 
+
+"""
+# Inner distances of neuron activations
+
 inners0001 = np.load('./results/evaluation/' + data_set_name + '/inference-0.001/' + setting + '_inner_dist_mean.npy')
 inners001 = np.load('./results/evaluation/' + data_set_name + '/inference-0.01/' + setting + '_inner_dist_mean.npy')
 inners01 = np.load('./results/evaluation/' + data_set_name + '/inference-0.1/' + setting + '_inner_dist_mean.npy')
@@ -50,9 +54,7 @@ plt.savefig(os.path.join(eval_dir, "inner-distance-per-H.png"))
 
 
 # check, are they all the same scale due to scaling??? should be at least m=0 and v=1
-
-
-
+"""
 
 
 
@@ -112,4 +114,78 @@ plt.close()
 
 """
 
+# plot the inferred initial states and the training initial states
+colors = ['red', 'orange', 'green', 'blue', 'gray', 'black']
+pattern_category = ['FACE', 'HOUSE', 'CAR', 'FLOWER', 'HUMAN', 'ROCKET']
+all_runs = ["2019-11-05_14-34_0970831", "2019-11-05_14-35_0321645", "2019-11-05_14-35_0421490", "2019-11-05_14-35_0786428", "2019-11-05_14-36_0715535", "2019-11-08_15-35_0902631", "2019-11-08_15-36_0061932", "2019-11-08_15-36_0110189", "2019-11-08_15-36_0712878", "2019-11-08_15-36_0818585"]
+setting = "pca-only-one-norm"
+num_inferences = 10
+num_patterns = 6
+#which_H = ['0.001', '0.01', '1', '100', '1000']
+which_H = ['0.001', '1', '1000']
+patterns_to_plot = [0, 1, 3]
+# from mpl_toolkits.mplot3d import Axes3D
 
+for plot_run in all_runs:
+    which_run = plot_run
+
+    fig = plt.figure('Trained and inferred IS', figsize=(10*(len(which_H)+1), 12.0))
+    plt.rcParams.update({'font.size': 40, 'legend.fontsize': 40})
+
+    for h in which_H:
+
+        # ax = fig.add_subplot(101 + len(which_H)*10 + which_H.index(h), projection='3d')
+        ax = fig.add_subplot(101 + len(which_H)*10 + which_H.index(h))
+        ax.set_title('H='+h)
+        ax.set_xlabel('PC 1')
+        ax.set_ylabel('PC 2')
+
+        trained_is = np.load(os.path.join(eval_dir, 'inference-' + h + '/' + setting + '_training-is_' + which_run + '_H-' + h + '.npy'))
+        ax.scatter(trained_is[:,0], trained_is[:,1], color= colors, s=1000,marker='*')
+        # ax.scatter(trained_is[:,0], trained_is[:,1], trained_is[:,2], color= colors, s=1000,marker='*')
+
+        for inf in range(num_inferences):
+            inferred_is = np.load(os.path.join(eval_dir, 'inference-' + h + '/' + setting + '_inferred-is_' + which_run + '-' + str(inf) + '_H-' + h + '.npy'))
+            ax.scatter(inferred_is[:,0], inferred_is[:,1], color= colors, s=200,marker='o')
+            # ax.scatter(inferred_is[:,0], inferred_is[:,1], inferred_is[:,2], color= colors, s=200,marker='o')
+
+    #fig.legend(loc=(0.855, 0.47))
+    fig.tight_layout()
+    fig.show()
+    # import pdb; pdb.set_trace()
+    plt.savefig(os.path.join(eval_dir, 'inferred-is-plot-' + str(len(which_H)) + '-' + which_run + '.pdf'))
+    plt.close()
+
+    # plot the neuron activations
+    fig = plt.figure('Neural activation trajectories', figsize=(10*(len(which_H)+1), 24.0))
+    plt.rcParams.update({'font.size': 40, 'legend.fontsize': 40})
+
+    for h in which_H:
+        # above row: 0:30 timesteps
+        ax = fig.add_subplot(201 + len(which_H)*10 + which_H.index(h))
+        ax.set_title('H='+h)
+        ax.set_xlabel('PC 1')
+        ax.set_ylabel('PC 2')
+
+        for inf in range(num_inferences):
+            for pat in patterns_to_plot:
+                uh_history = np.load(os.path.join(eval_dir, 'inference-' + h + '/' + setting + '_uh-history_' + which_run + '-' + str(inf) + '_pattern-' + str(pat) + '_H-' + h + '.npy'))
+                ax.scatter(uh_history[1:30,0], uh_history[1:30,1], color=colors[pat], s = 50, marker = 'o')
+                ax.scatter(uh_history[0,0], uh_history[0,1], color=colors[pat], s = 2000, marker = '*')
+                ax.scatter(uh_history[29,0], uh_history[29,1], color=colors[pat], s = 1000, marker = 's')
+
+        # lower row: 30:90 time_steps
+        ax = fig.add_subplot(201 + len(which_H)*10 + which_H.index(h) + 3)
+        ax.set_title('H='+h)
+        ax.set_xlabel('PC 1')
+        ax.set_ylabel('PC 2')
+
+        for inf in range(num_inferences):
+            for pat in patterns_to_plot:
+                uh_history = np.load(os.path.join(eval_dir, 'inference-' + h + '/' + setting + '_uh-history_' + which_run + '-' + str(inf) + '_pattern-' + str(pat) + '_H-' + h + '.npy'))
+                ax.scatter(uh_history[30:,0], uh_history[30:,1], color=colors[pat], s = 50, marker = 'o')
+                ax.scatter(uh_history[30,0], uh_history[30,1], color=colors[pat], s = 1000, marker = 's')
+
+    fig.tight_layout()
+    plt.savefig(os.path.join(eval_dir, 'uh-history-' + str(len(which_H)) + "-" + which_run + '.pdf'))
+    plt.close()
