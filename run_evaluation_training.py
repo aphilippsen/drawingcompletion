fcbffa0d46647696a7068222ea6cf1a9887ef905
Anja Philippsen <anja-philippsen@posteo.de>
2019-12-20 09:10:11 +0900

    add reactive results and plot

--- run_evaluation_training.py
+++ run_evaluation_training.py
@@ -1,5 +1,7 @@
 import os
 import numpy as np
+from nets import load_network
+
 
 train_hyp_all = ['0.001', '0.01', '0.1', '1', '10', '100', '1000']
 max_epoch = 30000
@@ -7,39 +9,89 @@ data_set_name = '2019-11-all'
 eval_head_dir = './results/training/' + data_set_name
 
 error_training_proactive = np.empty((len(train_hyp_all),), dtype=object)
+error_training_reactive = np.empty((len(train_hyp_all),), dtype=object)
 
 dir_list = next(os.walk(eval_head_dir))[1]
 for train_hyp in train_hyp_all:
     error_training_proactive[train_hyp_all.index(train_hyp)] = np.zeros((len(dir_list),np.int(max_epoch/100)+2))+1000 # initially set to some high value
+    error_training_reactive[train_hyp_all.index(train_hyp)] = np.zeros((len(dir_list),np.int(max_epoch/100)+2))+1000 # initially set to some high value
 
     for curr_r in range(len(dir_list)):
         current_run = os.path.join(eval_head_dir, dir_list[curr_r])
         current_net = os.path.join(current_run, train_hyp)
         print("Evaluate for " + str(current_net))
 
-        current_error = np.load(os.path.join(current_net, 'history_generation_error_proactive.npy'))
+        current_error_pro = np.load(os.path.join(current_net, 'history_generation_error_proactive.npy'))
+        current_error_re = np.load(os.path.join(current_net, 'history_generation_error_reactive.npy'))
 
-        mean_error_over_time = np.mean(np.concatenate(current_error, axis=0).reshape((6,-1)),axis=0)
+        mean_error_over_time_pro = np.mean(np.concatenate(current_error_pro, axis=0).reshape((6,-1)),axis=0)
+        mean_error_over_time_re = np.mean(np.concatenate(current_error_re, axis=0).reshape((6,-1)),axis=0)
 
-        error_training_proactive[train_hyp_all.index(train_hyp)][curr_r,:len(mean_error_over_time)] = mean_error_over_time
+        error_training_proactive[train_hyp_all.index(train_hyp)][curr_r,:len(mean_error_over_time_pro)] = mean_error_over_time_pro
+        error_training_reactive[train_hyp_all.index(train_hyp)][curr_r,:len(mean_error_over_time_re)] = mean_error_over_time_re
 
         # find the best epoch and set everything afterwards to that error
         min_idx = np.argmin(error_training_proactive[train_hyp_all.index(train_hyp)][curr_r,:])
         error_training_proactive[train_hyp_all.index(train_hyp)][curr_r,min_idx+1:] = error_training_proactive[train_hyp_all.index(train_hyp)][curr_r,min_idx]
 
-all_error_mean = np.zeros((len(train_hyp_all), np.int(max_epoch/100)+2))
-all_error_std = np.zeros((len(train_hyp_all), np.int(max_epoch/100)+2))
+        min_idx = np.argmin(error_training_reactive[train_hyp_all.index(train_hyp)][curr_r,:])
+        error_training_reactive[train_hyp_all.index(train_hyp)][curr_r,min_idx+1:] = error_training_reactive[train_hyp_all.index(train_hyp)][curr_r,min_idx]
+
+
+all_error_pro_mean = np.zeros((len(train_hyp_all), np.int(max_epoch/100)+2))
+all_error_pro_std = np.zeros((len(train_hyp_all), np.int(max_epoch/100)+2))
+all_error_re_mean = np.zeros((len(train_hyp_all), np.int(max_epoch/100)+2))
+all_error_re_std = np.zeros((len(train_hyp_all), np.int(max_epoch/100)+2))
 
 for train_hyp in train_hyp_all:
-    all_error_mean[train_hyp_all.index(train_hyp)] = np.mean(error_training_proactive[train_hyp_all.index(train_hyp)],axis=0)
-    all_error_std[train_hyp_all.index(train_hyp)] = np.std(error_training_proactive[train_hyp_all.index(train_hyp)],axis=0)
-
+    all_error_pro_mean[train_hyp_all.index(train_hyp)] = np.mean(error_training_proactive[train_hyp_all.index(train_hyp)],axis=0)
+    all_error_pro_std[train_hyp_all.index(train_hyp)] = np.std(error_training_proactive[train_hyp_all.index(train_hyp)],axis=0)
+    all_error_re_mean[train_hyp_all.index(train_hyp)] = np.mean(error_training_reactive[train_hyp_all.index(train_hyp)],axis=0)
+    all_error_re_std[train_hyp_all.index(train_hyp)] = np.std(error_training_reactive[train_hyp_all.index(train_hyp)],axis=0)
 
     # write results to file
-    with open(os.path.join(eval_head_dir, "training-error-" + str(train_hyp) + ".txt"), 'w') as f:
+    with open(os.path.join(eval_head_dir, "training-error-proactive-" + str(train_hyp) + ".txt"), 'w') as f:
         f.write("ep\tmean\tstd\n")
         for ep in range(np.int(max_epoch/100)+2):
-            f.write(str(ep) + "\t" + str(all_error_mean[train_hyp_all.index(train_hyp),ep]) + "\t" + str(all_error_std[train_hyp_all.index(train_hyp),ep]) + "\n")
+            f.write(str(ep) + "\t" + str(all_error_pro_mean[train_hyp_all.index(train_hyp),ep]) + "\t" + str(all_error_pro_std[train_hyp_all.index(train_hyp),ep]) + "\n")
+
+    # write results to file
+    with open(os.path.join(eval_head_dir, "training-error-reactive-" + str(train_hyp) + ".txt"), 'w') as f:
+        f.write("ep\tmean\tstd\n")
+        for ep in range(np.int(max_epoch/100)+2):
+            f.write(str(ep) + "\t" + str(all_error_re_mean[train_hyp_all.index(train_hyp),ep]) + "\t" + str(all_error_re_std[train_hyp_all.index(train_hyp),ep]) + "\n")
 
 
+
+# generate and plot the learned trajectories, using the best epoch
+import matplotlib.pyplot as plt
+dir_list = next(os.walk(eval_head_dir))[1]
+for train_hyp in train_hyp_all:
+
+    # plot all results for one H
+    plt.figure()
+    fig = plt.figure('Proactive results', figsize=(30, 60.0))
+    plt.rcParams.update({'font.size': 40, 'legend.fontsize': 40})
+
+    for curr_r in range(len(dir_list)):
+
+        current_run = os.path.join(eval_head_dir, dir_list[curr_r])
+        current_net = os.path.join(current_run, train_hyp)
+
+        params, model = load_network(current_net, model_filename="network-epoch-best")
+
+        # proactive
+        res, resv, resm, u_h_history = model.generate(model.initial_states.W.array, 90, epsilon_disturbance = 0, additional_output='activations', external_signal_variance = model.external_signal_variance)
+                
+        for i in range(res.shape[0]):
+            ax = fig.add_subplot(10,6,curr_r*6+(i+1))
+            traj = res[i,:].reshape((90,3))
+            ax.plot(traj[:,0], traj[:,1])
+            
+    plt.tight_layout()
+    plt.savefig('proactive-results-H-' + str(train_hyp) + ".pdf")
+    plt.close()
+
+        # reactive
+#res, resv, resm, pe, wpe, u_h_history, respos = model.generate(model.initial_states.W.array, num_timesteps, external_input = xp.copy(xp.asarray(x_train[:test_batch_size,:])), epsilon_disturbance = 0, additional_output='activations', external_signal_variance = explicit_sensor_variance)
 
--- test_plot.py
+++ test_plot.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-data_set_name = '2019-11-all'
+data_set_name = '2019-11-all-new'
 setting = "pca-only-one-norm"
 eval_dir = "./results/evaluation/" + data_set_name + "/"
 
@@ -114,6 +114,7 @@ plt.close()
 
 """
 
+"""
 # plot the inferred initial states and the training initial states
 colors = ['red', 'orange', 'green', 'blue', 'gray', 'black']
 pattern_category = ['FACE', 'HOUSE', 'CAR', 'FLOWER', 'HUMAN', 'ROCKET']
@@ -156,6 +157,8 @@ for plot_run in all_runs:
     plt.savefig(os.path.join(eval_dir, 'inferred-is-plot-' + str(len(which_H)) + '-' + which_run + '.pdf'))
     plt.close()
 
+
+
     # plot the neuron activations
     fig = plt.figure('Neural activation trajectories', figsize=(10*(len(which_H)+1), 24.0))
     plt.rcParams.update({'font.size': 40, 'legend.fontsize': 40})
@@ -173,6 +176,10 @@ for plot_run in all_runs:
                 ax.scatter(uh_history[1:30,0], uh_history[1:30,1], color=colors[pat], s = 50, marker = 'o')
                 ax.scatter(uh_history[0,0], uh_history[0,1], color=colors[pat], s = 2000, marker = '*')
                 ax.scatter(uh_history[29,0], uh_history[29,1], color=colors[pat], s = 1000, marker = 's')
+        ax.set_xlim([-1,1])
+        ax.set_ylim([-1,1])
+        ax.set_xticks([-1,-0.5,0,0.5,1])
+        ax.set_yticks([-1,-0.5,0,0.5,1])
 
         # lower row: 30:90 time_steps
         ax = fig.add_subplot(201 + len(which_H)*10 + which_H.index(h) + 3)
@@ -180,12 +187,279 @@ for plot_run in all_runs:
         ax.set_xlabel('PC 1')
         ax.set_ylabel('PC 2')
 
+
         for inf in range(num_inferences):
             for pat in patterns_to_plot:
                 uh_history = np.load(os.path.join(eval_dir, 'inference-' + h + '/' + setting + '_uh-history_' + which_run + '-' + str(inf) + '_pattern-' + str(pat) + '_H-' + h + '.npy'))
                 ax.scatter(uh_history[30:,0], uh_history[30:,1], color=colors[pat], s = 50, marker = 'o')
                 ax.scatter(uh_history[30,0], uh_history[30,1], color=colors[pat], s = 1000, marker = 's')
+        ax.set_xlim([-1,1])
+        ax.set_ylim([-1,1])
+        ax.set_xticks([-1,-0.5,0,0.5,1])
+        ax.set_yticks([-1,-0.5,0,0.5,1])
 
     fig.tight_layout()
     plt.savefig(os.path.join(eval_dir, 'uh-history-' + str(len(which_H)) + "-" + which_run + '.pdf'))
     plt.close()
+
+
+
+    # plot initial state emergence
+    fig = plt.figure('Neural activation trajectories', figsize=(10*(len(which_H)+1), 12.0))
+    plt.rcParams.update({'font.size': 40, 'legend.fontsize': 40})
+
+    for h in which_H:
+        # above row: 0:30 timesteps
+        ax = fig.add_subplot(101 + len(which_H)*10 + which_H.index(h))
+        ax.set_title('H='+h)
+        ax.set_xlabel('PC 1')
+        ax.set_ylabel('PC 2')
+
+        for inf in range(num_inferences):
+            for pat in patterns_to_plot:
+                is_history = np.load(os.path.join(eval_dir, 'inference-' + h + '/' + setting + '_is-history_' + which_run + '-' + str(inf) + '_pattern-' + str(pat) + '_H-' + h + '.npy'))
+                ax.scatter(is_history[1:30,0], is_history[1:30,1], color=colors[pat], s = 50, marker = 'o')
+                ax.scatter(is_history[0,0], is_history[0,1], color=colors[pat], s = 2000, marker = '*')
+                ax.scatter(is_history[is_history.shape[0]-1,0], is_history[is_history.shape[0]-1,1], color=colors[pat], s = 1000, marker = 's')
+
+    fig.tight_layout()
+    plt.savefig(os.path.join(eval_dir, 'is-history-' + str(len(which_H)) + "-" + which_run + '.pdf'))
+    plt.close()
+"""
+
+
+"""
+disttotrain0001 = np.load(eval_dir + '/inference-0.001/' + setting + '_dist_to_corresponding_training_is.npy')
+disttotrain001 = np.load(eval_dir + '/inference-0.01/' + setting + '_dist_to_corresponding_training_is.npy')
+disttotrain01 = np.load(eval_dir + '/inference-0.1/' + setting + '_dist_to_corresponding_training_is.npy')
+disttotrain1 = np.load(eval_dir + '/inference-1/' + setting + '_dist_to_corresponding_training_is.npy')
+disttotrain10 = np.load(eval_dir + '/inference-10/' + setting + '_dist_to_corresponding_training_is.npy')
+disttotrain100 = np.load(eval_dir + '/inference-100/' + setting + '_dist_to_corresponding_training_is.npy')
+disttotrain1000 = np.load(eval_dir + '/inference-1000/' + setting + '_dist_to_corresponding_training_is.npy')
+
+distbettrain0001 = np.load(eval_dir + '/inference-0.001/' + setting + '_dist_between_training_is.npy')
+distbettrain001 = np.load(eval_dir + '/inference-0.01/' + setting + '_dist_between_training_is.npy')
+distbettrain01 = np.load(eval_dir + '/inference-0.1/' + setting + '_dist_between_training_is.npy')
+distbettrain1 = np.load(eval_dir + '/inference-1/' + setting + '_dist_between_training_is.npy')
+distbettrain10 = np.load(eval_dir + '/inference-10/' + setting + '_dist_between_training_is.npy')
+distbettrain100 = np.load(eval_dir + '/inference-100/' + setting + '_dist_between_training_is.npy')
+distbettrain1000 = np.load(eval_dir + '/inference-1000/' + setting + '_dist_between_training_is.npy')
+
+plt.figure()
+
+for i in range(disttotrain0001.shape[0]):
+    for j in range(len(disttotrain0001[0])):
+        plt.plot(1, disttotrain0001[i][j], color='blue', marker='*')
+        plt.plot(2, disttotrain001[i][j], color='purple', marker='*')
+        plt.plot(3, disttotrain01[i][j], color='turquoise', marker='*')
+        plt.plot(4, disttotrain1[i][j], color='green', marker='*')
+        plt.plot(5, disttotrain10[i][j], color='orange', marker='*')
+        plt.plot(6, disttotrain100[i][j], color='red', marker='*')
+        plt.plot(7, disttotrain1000[i][j], color='pink', marker='*')
+plt.savefig(os.path.join(eval_dir, "disttotrain.png"))
+
+plt.figure()
+
+for i in range(distbettrain0001.shape[0]):
+    plt.plot(1, distbettrain0001[i], color='blue', marker='*')
+    plt.plot(2, distbettrain001[i], color='purple', marker='*')
+    plt.plot(3, distbettrain01[i], color='turquoise', marker='*')
+    plt.plot(4, distbettrain1[i], color='green', marker='*')
+    plt.plot(5, distbettrain10[i], color='orange', marker='*')
+    plt.plot(6, distbettrain100[i], color='red', marker='*')
+    plt.plot(7, distbettrain1000[i], color='pink', marker='*')
+plt.savefig(os.path.join(eval_dir, "distbettrain.png"))
+
+
+#plt.errorbar(np.arange(90), np.mean(outers0001,axis=0), yerr=np.mean(outers0001v,axis=0), color='blue')
+#plt.errorbar(np.arange(90), np.mean(outers001,axis=0), yerr=np.mean(outers001v,axis=0), color='purple')
+#plt.errorbar(np.arange(90), np.mean(outers01,axis=0), yerr=np.mean(outers01v,axis=0), color='turquoise')
+#plt.errorbar(np.arange(90), np.mean(outers1,axis=0), yerr=np.mean(outers1v,axis=0), color='green')
+#plt.errorbar(np.arange(90), np.mean(outers10,axis=0), yerr=np.mean(outers10v,axis=0), color='orange')
+#plt.errorbar(np.arange(90), np.mean(outers100,axis=0), yerr=np.mean(outers100v,axis=0), color='red')
+#plt.errorbar(np.arange(90), np.mean(outers1000,axis=0), yerr=np.mean(outers1000v,axis=0), color='pink')
+
+"""
+
+
+
+
+
+
+
+"""
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+data_set_name = '2019-11-all'
+setting = "pca-only-one-norm"
+eval_dir = "./results/evaluation/" + data_set_name + "/"
+
+inners0001 = np.load(eval_dir + '/inference-0.001/' + setting + '_inner_dist_mean.npy')
+inners001 = np.load(eval_dir + '/inference-0.01/' + setting + '_inner_dist_mean.npy')
+inners01 = np.load(eval_dir + '/inference-0.1/' + setting + '_inner_dist_mean.npy')
+inners1 = np.load(eval_dir + '/inference-1/' + setting + '_inner_dist_mean.npy')
+inners10 = np.load(eval_dir + '/inference-10/' + setting + '_inner_dist_mean.npy')
+inners100 = np.load(eval_dir + '/inference-100/' + setting + '_inner_dist_mean.npy')
+inners1000 = np.load(eval_dir + '/inference-1000/' + setting + '_inner_dist_mean.npy')
+
+inners0001v = np.load(eval_dir + '/inference-0.001/' + setting + '_inner_dist_var.npy')
+inners001v = np.load(eval_dir + '/inference-0.01/' + setting + '_inner_dist_var.npy')
+inners01v = np.load(eval_dir + '/inference-0.1/' + setting + '_inner_dist_var.npy')
+inners1v = np.load(eval_dir + '/inference-1/' + setting + '_inner_dist_var.npy')
+inners10v = np.load(eval_dir + '/inference-10/' + setting + '_inner_dist_var.npy')
+inners100v = np.load(eval_dir + '/inference-100/' + setting + '_inner_dist_var.npy')
+inners1000v = np.load(eval_dir + '/inference-1000/' + setting + '_inner_dist_var.npy')
+
+
+outers0001 = np.load(eval_dir + '/inference-0.001/' + setting + '_outer_dist_mean.npy')
+outers001 = np.load(eval_dir + '/inference-0.01/' + setting + '_outer_dist_mean.npy')
+outers01 = np.load(eval_dir + '/inference-0.1/' + setting + '_outer_dist_mean.npy')
+outers1 = np.load(eval_dir + '/inference-1/' + setting + '_outer_dist_mean.npy')
+outers10 = np.load(eval_dir + '/inference-10/' + setting + '_outer_dist_mean.npy')
+outers100 = np.load(eval_dir + '/inference-100/' + setting + '_outer_dist_mean.npy')
+outers1000 = np.load(eval_dir + '/inference-1000/' + setting + '_outer_dist_mean.npy')
+
+outers0001v = np.load(eval_dir + '/inference-0.001/' + setting + '_outer_dist_var.npy')
+outers001v = np.load(eval_dir + '/inference-0.01/' + setting + '_outer_dist_var.npy')
+outers01v = np.load(eval_dir + '/inference-0.1/' + setting + '_outer_dist_var.npy')
+outers1v = np.load(eval_dir + '/inference-1/' + setting + '_outer_dist_var.npy')
+outers10v = np.load(eval_dir + '/inference-10/' + setting + '_outer_dist_var.npy')
+outers100v = np.load(eval_dir + '/inference-100/' + setting + '_outer_dist_var.npy')
+outers1000v = np.load(eval_dir + '/inference-1000/' + setting + '_outer_dist_var.npy')
+
+# This is variability between patterns which is not so interesting here
+#outers100var = np.load('./results/evaluation/' + data_set_name + '/inference-100/' + setting = "pca-only-one" + '_outer_dist_var.npy')
+#outers10var = np.load('./results/evaluation/' + data_set_name + '/inference-10/' + setting = "pca-only-one" + '_outer_dist_var.npy')
+#outers1var = np.load('./results/evaluation/' + data_set_name + '/inference-1/' + setting = "pca-only-one" + '_outer_dist_var.npy')
+#outers001var = np.load('./results/evaluation/' + data_set_name + '/inference-0.01/' + setting = "pca-only-one" + '_outer_dist_var.npy')
+#outers01var = np.load('./results/evaluation/' + data_set_name + '/inference-0.1/' + setting = "pca-only-one" + '_outer_dist_var.npy')
+
+colors = ['blue', 'purple', 'turquoise', 'green', 'red', 'orange', 'pink']
+
+plt.figure()
+
+for i in range(outers001.shape[0]):
+    plt.plot(np.arange(90), outers0001[i,:], color='blue')
+    plt.plot(np.arange(90), outers001[i,:], color='purple')
+    plt.plot(np.arange(90), outers01[i,:], color='turquoise')
+    plt.plot(np.arange(90), outers1[i,:], color='green')
+    plt.plot(np.arange(90), outers10[i,:], color='orange')
+    plt.plot(np.arange(90), outers100[i,:], color='red')
+    plt.plot(np.arange(90), outers1000[i,:], color='pink')
+
+#plt.errorbar(np.arange(90), np.mean(outers0001,axis=0), yerr=np.mean(outers0001v,axis=0), color='blue')
+#plt.errorbar(np.arange(90), np.mean(outers001,axis=0), yerr=np.mean(outers001v,axis=0), color='purple')
+#plt.errorbar(np.arange(90), np.mean(outers01,axis=0), yerr=np.mean(outers01v,axis=0), color='turquoise')
+#plt.errorbar(np.arange(90), np.mean(outers1,axis=0), yerr=np.mean(outers1v,axis=0), color='green')
+#plt.errorbar(np.arange(90), np.mean(outers10,axis=0), yerr=np.mean(outers10v,axis=0), color='orange')
+#plt.errorbar(np.arange(90), np.mean(outers100,axis=0), yerr=np.mean(outers100v,axis=0), color='red')
+#plt.errorbar(np.arange(90), np.mean(outers1000,axis=0), yerr=np.mean(outers1000v,axis=0), color='pink')
+
+plt.savefig(os.path.join(eval_dir, "outer-distance-per-H.png"))
+
+
+# export separately all 0.01... etc per time scale... no better do that already in the eval-repr script
+
+
+# check, are they all the same scale due to scaling??? should be at least m=0 and v=1
+
+
+
+disttotrain0001 = np.load(eval_dir + '/inference-0.001/' + setting + '_dist_to_corresponding_training_is.npy')
+disttotrain001 = np.load(eval_dir + '/inference-0.01/' + setting + '_dist_to_corresponding_training_is.npy')
+disttotrain01 = np.load(eval_dir + '/inference-0.1/' + setting + '_dist_to_corresponding_training_is.npy')
+disttotrain1 = np.load(eval_dir + '/inference-1/' + setting + '_dist_to_corresponding_training_is.npy')
+disttotrain10 = np.load(eval_dir + '/inference-10/' + setting + '_dist_to_corresponding_training_is.npy')
+disttotrain100 = np.load(eval_dir + '/inference-100/' + setting + '_dist_to_corresponding_training_is.npy')
+disttotrain1000 = np.load(eval_dir + '/inference-1000/' + setting + '_dist_to_corresponding_training_is.npy')
+
+distbettrain0001 = np.load(eval_dir + '/inference-0.001/' + setting + '_dist_between_training_is.npy')
+distbettrain001 = np.load(eval_dir + '/inference-0.01/' + setting + '_dist_between_training_is.npy')
+distbettrain01 = np.load(eval_dir + '/inference-0.1/' + setting + '_dist_between_training_is.npy')
+distbettrain1 = np.load(eval_dir + '/inference-1/' + setting + '_dist_between_training_is.npy')
+distbettrain10 = np.load(eval_dir + '/inference-10/' + setting + '_dist_between_training_is.npy')
+distbettrain100 = np.load(eval_dir + '/inference-100/' + setting + '_dist_between_training_is.npy')
+distbettrain1000 = np.load(eval_dir + '/inference-1000/' + setting + '_dist_between_training_is.npy')
+
+plt.figure()
+
+for i in range(disttotrain0001.shape[0]):
+    for j in range(len(disttotrain0001[0])):
+        plt.plot(1, disttotrain0001[i][j]/distbettrain0001[i], color='blue', marker='*')
+        plt.plot(2, disttotrain001[i][j]/distbettrain001[i], color='purple', marker='*')
+        plt.plot(3, disttotrain01[i][j]/distbettrain01[i], color='turquoise', marker='*')
+        plt.plot(4, disttotrain1[i][j]/distbettrain1[i], color='green', marker='*')
+        plt.plot(5, disttotrain10[i][j]/distbettrain10[i], color='orange', marker='*')
+        plt.plot(6, disttotrain100[i][j]/distbettrain100[i], color='red', marker='*')
+        plt.plot(7, disttotrain1000[i][j]/distbettrain1000[i], color='pink', marker='*')
+
+#plt.errorbar(np.arange(90), np.mean(outers0001,axis=0), yerr=np.mean(outers0001v,axis=0), color='blue')
+#plt.errorbar(np.arange(90), np.mean(outers001,axis=0), yerr=np.mean(outers001v,axis=0), color='purple')
+#plt.errorbar(np.arange(90), np.mean(outers01,axis=0), yerr=np.mean(outers01v,axis=0), color='turquoise')
+#plt.errorbar(np.arange(90), np.mean(outers1,axis=0), yerr=np.mean(outers1v,axis=0), color='green')
+#plt.errorbar(np.arange(90), np.mean(outers10,axis=0), yerr=np.mean(outers10v,axis=0), color='orange')
+#plt.errorbar(np.arange(90), np.mean(outers100,axis=0), yerr=np.mean(outers100v,axis=0), color='red')
+#plt.errorbar(np.arange(90), np.mean(outers1000,axis=0), yerr=np.mean(outers1000v,axis=0), color='pink')
+
+plt.savefig(os.path.join(eval_dir, "disttotrain.png"))
+
+
+# PLOT TRAINING DATA
+
+train_data = np.load('data/drawing-data-sets/drawings-191105-6-drawings.npy')
+num_timesteps = 90
+input_dim = 3
+draw_only = 30
+num_classes = 6
+# 3rd dim == 1 means that this point is connected to the previous one, dim == 0 that not!
+
+fig = plt.figure('Original training data', figsize=(30, 21.0))
+plt.rcParams.update({'font.size': 55, 'legend.fontsize': 30})
+
+
+names = ['FACE', 'HOUSE', 'CAR', 'FLOWER', 'HUMAN', 'ROCKET']
+for pattern in range(num_classes):
+    ax = fig.add_subplot(231+pattern)
+    ax.set_title(names[pattern])
+    ax.set_xlabel('$x_0$')
+    ax.set_ylabel('$x_1$')
+    ax.set_xlim([-1.1, 1.1])
+    ax.set_ylim([-1.1, 1.1])
+    for i in range(pattern, train_data.shape[0], num_classes):
+        for t in range(1, num_timesteps):
+            traj = train_data[i,:].reshape((num_timesteps, input_dim))
+            if int(np.round(traj[t,2])) == 1:
+                if t < draw_only:
+                    ax.plot(traj[t-1:t+1,0], traj[t-1:t+1,1], 'k', linewidth = 0.3)
+            else:
+                if t < draw_only:
+                    ax.plot(traj[t-1:t+1,0], traj[t-1:t+1,1], 'lightgray', linewidth = 0.3)
+    current_i=6+pattern
+    for t in range(1, num_timesteps):
+        traj = train_data[current_i,:].reshape((num_timesteps, input_dim))
+        if int(np.round(traj[t,2])) == 1:
+            if t < draw_only:
+                ax.plot(traj[t-1:t+1,0], traj[t-1:t+1,1], 'k', linewidth = 5)
+        else:
+            if t < draw_only:
+                ax.plot(traj[t-1:t+1,0], traj[t-1:t+1,1], 'lightgray', linewidth = 5)
+
+    if pattern == 1 or pattern == 2 or pattern == 4 or pattern == 5:
+        plt.yticks([], [])
+        plt.ylabel('')
+    if pattern <= 2:
+        plt.xticks([], [])
+        plt.xlabel('')
+
+
+plt.tight_layout()
+#plt.savefig('original-training-data.pdf')
+plt.savefig('to-complete-training-data.pdf')
+plt.close()
+
+"""
+
+
